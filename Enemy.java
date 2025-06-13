56ec90b1870bc987572474325b7d4189d12861c8
Thomas Gimenez <thomas.gimenez31@gmail.com>
2025-06-13 17:16:47 +0200

    Added Enemy

--- Enemy.java
+++ Enemy.java
@@ -1,7 +1,32 @@
 package xXxDungeon_EscapexXx;
 
-public class Enemy {
-    
-}
+import java.util.Random;
 
-//test test
+public class Enemy extends Entity {
+    private final int xpReward;
+    private final int minDamage;
+    private final int maxDamage;
+
+    public Enemy(String name, int hp, int speed, int xpReward, int minDamage, int maxDamage) {
+        super(name, hp, speed);
+        this.xpReward = xpReward;
+        this.minDamage = minDamage;
+        this.maxDamage = maxDamage;
+    }
+
+    public int getXpReward() {
+        return xpReward;
+    }
+
+    public int rollDamage() {
+        Random random = new Random();
+        if (maxDamage <= minDamage) {
+            return minDamage;
+        }
+        return random.nextInt(maxDamage - minDamage + 1) + minDamage;
+    }
+
+    public String toString() {
+        return String.format("%s (HP: %d, SPD: %d)", name, hp, speed);
+    }
+}
