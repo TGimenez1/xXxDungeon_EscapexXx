bdd0cda627efa5b60e2c92c0dd741b957021d6b5
PvsSociety <paulvincent.mbanzoulou.pro@gmail.com>
2025-06-19 09:55:29 +0200

    Modification of player.java (Add Inventory and import equipable.java)

--- Player.java
+++ Player.java
@@ -1,89 +1,126 @@
 package xXxDungeon_EscapexXx;
 
+
 import java.util.Random;
+import xXxDungeon_EscapexXx.Equipable;
+
 
 public class Player extends Entity {
-    private Equipable equippedRight;
-    private Equipable equippedLeft;
-    private int xp;
-    private int playerLevel;
-    private static final int XP_PER_LEVEL = 100;
+   private Equipable equippedRight;
+   private Equipable equippedLeft;
+   private Inventory  inventory;
 
-    public Player(String name, int hp, int speed) {
-        super(name, hp, speed);
-        this.equippedRight = null;
-        this.equippedLeft = null;
-        this.xp = 0;
-        this.playerLevel = 1;
-    }
 
-    public Equipable getEquippedRight() {
-        return equippedRight;
-    }
+   private int xp;
+   private int playerLevel;
+   private static final int XP_PER_LEVEL = 100;
 
-    public void equipRight(Equipable item) {
-        this.equippedRight = item;
-    }
 
-    public Equipable unequipRight() {
-        Equipable old = equippedRight;
-        this.equippedRight = null;
-        return old;
-    }
+   public Player(String name, int hp, int speed) {
+       super(name, hp, speed);
+       this.equippedRight = null;
+       this.equippedLeft  = null;
+       this.inventory     = new Inventory();
+       this.xp            = 0;
+       this.playerLevel   = 1;
+   }
 
-    public Equipable getEquippedLeft() {
-        return equippedLeft;
-    }
 
-    public void equipLeft(Equipable item) {
-        this.equippedLeft = item;
-    }
+   public Equipable getEquippedRight() {
+       return equippedRight;
+   }
 
-    public Equipable unequipLeft() {
-        Equipable old = equippedLeft;
-        this.equippedLeft = null;
-        return old;
-    }
 
-    public int getXp() {
-        return xp;
-    }
+   public void equipRight(Equipable item) {
+       if (equippedRight != null) {
+           Equipable old = equippedRight;
+           old.unequip(this);
+           inventory.put(old);
+       }
+       item.equip(this);
+       this.equippedRight = item;
+   }
 
-    public int getPlayerLevel() {
-        return playerLevel;
-    }
 
-    public void gainXp(int amount) {
-        xp += amount;
-        checkLevelUp();
-    }
+   public Equipable unequipRight() {
+       if (equippedRight == null) return null;
+       Equipable old = equippedRight;
+       old.unequip(this);
+       inventory.put(old);
+       this.equippedRight = null;
+       return old;
+   }
 
-    private void checkLevelUp() {
-        int requiredXp = playerLevel * XP_PER_LEVEL;
-        while (xp >= requiredXp) {
-            xp -= requiredXp;
-            playerLevel++;
-            requiredXp = playerLevel * XP_PER_LEVEL;
-            onLevelUp();
-        }
-    }
 
-    protected void onLevelUp() {
-        hp += 10;
-        speed += 1;
-        System.out.println("Le joueur " + name + " est passé au niveau " + playerLevel + " !");
-    }
+   public Equipable getEquippedLeft() {
+       return equippedLeft;
+   }
 
-    /**
-     * Simulates an initiative roll
-     * Rolls between 70% and 100% of speed for both player and enemy.
-     * Returns true if the player wins ties against the enemy.
-     */
 
-    public boolean playerHasInitiative(int enemySpeed) {
-        Random random = new Random();
-        int playerRoll = random.nextInt(speed * 3 / 10 + 1) + speed * 7 / 10;
-        int enemyRoll = random.nextInt(enemySpeed * 3 / 10 + 1) + enemySpeed * 7 / 10;
-        return playerRoll >= enemyRoll;
-    }
+   public void equipLeft(Equipable item) {
+       if (equippedLeft != null) {
+           Equipable old = equippedLeft;
+           old.unequip(this);
+           inventory.put(old);
+       }
+       item.equip(this);
+       this.equippedLeft = item;
+   }
+
+
+   public Equipable unequipLeft() {
+       if (equippedLeft == null) return null;
+       Equipable old = equippedLeft;
+       old.unequip(this);
+       inventory.put(old);
+       this.equippedLeft = null;
+       return old;
+   }
+
+
+   public int getXp() {
+       return xp;
+   }
+
+
+   public int getPlayerLevel() {
+       return playerLevel;
+   }
+
+
+   public void gainXp(int amount) {
+       xp += amount;
+       checkLevelUp();
+   }
+
+
+   private void checkLevelUp() {
+       int requiredXp = playerLevel * XP_PER_LEVEL;
+       while (xp >= requiredXp) {
+           xp -= requiredXp;
+           playerLevel++;
+           requiredXp = playerLevel * XP_PER_LEVEL;
+           onLevelUp();
+       }
+   }
+
+
+   protected void onLevelUp() {
+       hp += 10;
+       speed += 1;
+       System.out.println("Le joueur " + name + " est passé au niveau " + playerLevel + " !");
+   }
+
+
+   /**
+    * Simulates an initiative roll
+    * Rolls between 70% and 100% of speed for both player and enemy.
+    * Returns true if the player wins ties against the enemy.
+    */
+   public boolean playerHasInitiative(int enemySpeed) {
+       Random random = new Random();
+       int playerRoll = random.nextInt(speed * 3 / 10 + 1) + speed * 7 / 10;
+       int enemyRoll  = random.nextInt(enemySpeed * 3 / 10 + 1) + enemySpeed * 7 / 10;
+       return playerRoll >= enemyRoll;
+   }
 }
